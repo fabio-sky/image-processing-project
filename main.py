1842e957b1496204398cd607cc3e6a585e755913
Fabio <fabio.cielo97@gmail.com>
2021-12-19 22:52:25 +0100

    Added border detection

--- main.py
+++ main.py
@@ -5,7 +5,11 @@ import matplotlib.pyplot as plt
 
 def main():
     print('MAIN')
-    readImage()
+    img = readImage()
+    img = cleanImage(img)
+    img = detectLeaf(img)
+    img = detectEdge(img)
+    plotImage(img)
 
 
 def readImage():
@@ -13,12 +17,34 @@ def readImage():
     location = "./images/quercia.jpg"
     leaf = cv2.imread(location)
     leaf = cv2.cvtColor(leaf, cv2.COLOR_BGR2RGB)
-    cleanImage()
-    plotImage(leaf)
+    return leaf
 
 
-def cleanImage():
+def cleanImage(image):
     print('CLEAN IMAGE')
+    img = cv2.GaussianBlur(image, (5, 5), 0)
+    return img
+
+
+def detectEdge(image):
+    img = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    edges = cv2.Canny(img, 150, 300)
+    indices = np.where(edges != [0])
+    coordinates = zip(indices[0], indices[1])
+    print(indices[0])
+    return edges
+
+def detectLeaf(img):
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    # find the brown color
+    mask_brown = cv2.inRange(hsv, (8, 60, 20), (30, 255, 200))
+    # find the yellow and green color in the leaf
+    mask_yellow_green = cv2.inRange(hsv, (10, 39, 64), (86, 255, 255))
+    # find any of the three colors(green or brown or yellow) in the image
+    mask = cv2.bitwise_or(mask_yellow_green, mask_brown)
+    # Bitwise-AND mask and original image
+    res = cv2.bitwise_and(img, img, mask=mask)
+    return res
 
 
 def plotImage(image):
